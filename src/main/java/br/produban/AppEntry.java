891adba6410ce5c40f61a5211bcbfe65692ecdea
Bera <brunormoura@gmail.com>
2016-06-23 13:20:15 -0300

    Basic routes and methods done for see rules, also the html view too

--- src/main/java/br/produban/AppEntry.java
+++ src/main/java/br/produban/AppEntry.java
@@ -1,9 +1,18 @@
 package br.produban;
 
+import br.produban.Entities.Rule;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.StaticHandler;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Hello world!
@@ -11,6 +20,9 @@ import io.vertx.ext.web.Router;
 public class AppEntry extends AbstractVerticle {
     @Override
     public void start(Future<Void> fut) {
+
+
+        createData();
 
         // Create a router object.
         Router router = Router.router(vertx);
@@ -22,6 +34,15 @@ public class AppEntry extends AbstractVerticle {
                     .putHeader("content-type", "text/html")
                     .end("<h1>Working</h1>");
         });
+
+        router.route("/assets/*").handler(StaticHandler.create("assets"));
+
+        router.get("/api/rules").handler(this::getAllRules);
+        router.route("/api/rules*").handler(BodyHandler.create());
+        router.post("/api/rules").handler(this::addRule);
+        router.get("/api/rules/:id").handler(this::getRule);
+        router.put("/api/rules/:id").handler(this::updateRule);
+        router.delete("/api/rules/:id").handler(this::deleteRule);
 
         vertx
                 .createHttpServer()
@@ -39,4 +60,99 @@ public class AppEntry extends AbstractVerticle {
                         }
                 );
     }
+
+    private void createData() {
+        Rule regra1 = new Rule(
+                "xb1825091352016111717889",
+                "xb182509",
+                "xb182509: (13/5/2016)-(11:17:17)",
+                "openbus_br_zabbix_v2",
+                null);
+        rules.put(regra1.getId(), regra1);
+        Rule regra2 = new Rule(
+                "xb1817531352016111717889",
+                "xb181753",
+                "xb181753: (13/5/2016)-(11:17:17)",
+                "openbus_br_vhyper_v1",
+                null);
+        rules.put(regra2.getId(), regra2);
+    }
+
+    private void addRule(RoutingContext routingContext) {
+        // Read the request's content and create an instance of Rule.
+        final Rule Rule = Json.decodeValue(routingContext.getBodyAsString(),
+                Rule.class);
+        // Add it to the backend map
+        rules.put(Rule.getId(), Rule);
+
+        // Return the created Rule as JSON
+        routingContext.response()
+                .setStatusCode(201)
+                .putHeader("content-type", "application/json; charset=utf-8")
+                .end(Json.encodePrettily(Rule));
+    }
+
+    private void getRule(RoutingContext routingContext) {
+        final String id = routingContext.request().getParam("id");
+        if (id == null) {
+            routingContext.response().setStatusCode(400).end();
+        } else {
+            final Integer idAsInteger = Integer.valueOf(id);
+            Rule Rule = rules.get(idAsInteger);
+            if (Rule == null) {
+                routingContext.response().setStatusCode(404).end();
+            } else {
+                routingContext.response()
+                        .putHeader("content-type", "application/json; charset=utf-8")
+                        .end(Json.encodePrettily(Rule));
+            }
+        }
+    }
+
+    private void updateRule(RoutingContext routingContext) {
+        final String id = routingContext.request().getParam("id");
+        JsonObject json = routingContext.getBodyAsJson();
+        if (id == null || json == null) {
+            routingContext.response().setStatusCode(400).end();
+        } else {
+            final Integer idAsInteger = Integer.valueOf(id);
+            Rule Rule = rules.get(idAsInteger);
+            if (Rule == null) {
+                routingContext.response().setStatusCode(404).end();
+            } else {
+                Rule.setTool(json.getString("tool"));
+                Rule.setCreated_by_user(json.getString("created_by_user"));
+                Rule.setEdited_by_user(json.getString("edited_by_user"));
+                Rule.setRuleid(json.getString("ruleid"));
+
+                routingContext.response()
+                        .putHeader("content-type", "application/json; charset=utf-8")
+                        .end(Json.encodePrettily(Rule));
+            }
+        }
+    }
+
+    private void deleteRule(RoutingContext routingContext) {
+        String id = routingContext.request().getParam("id");
+        if (id == null) {
+            routingContext.response().setStatusCode(400).end();
+        } else {
+            Integer idAsInteger = Integer.valueOf(id);
+            rules.remove(idAsInteger);
+        }
+        routingContext.response().setStatusCode(204).end();
+    }
+
+    private void getAllRules(RoutingContext routingContext) {
+        // Write the HTTP response
+        // The response is in JSON using the utf-8 encoding
+        // We returns the list of bottles
+        routingContext.response()
+                .putHeader("content-type", "application/json; charset=utf-8")
+                .end(Json.encodePrettily(rules.values()));
+    }
+
+
+    // Store rules
+    private Map<Integer, Rule> rules = new LinkedHashMap<>();
 }
--- src/main/java/br/produban/Entities/Rule.java
+++ src/main/java/br/produban/Entities/Rule.java
@@ -22,15 +22,17 @@ public class Rule {
 
     private String tool;
 
+    private HashMap<String,String> rule_filters;
+
     public int getId() {
         return id;
     }
 
-    public Rule(int id) {
+    public Rule() {
         this.id = COUNTER.getAndIncrement();
     }
-
-    public Rule(int id, String ruleid, String created_by_user, String edited_by_user, String tool, HashMap<String, String> rule_filters) {
+    public Rule(String ruleid, String created_by_user,
+                String edited_by_user, String tool, HashMap<String, String> rule_filters) {
         this.id = COUNTER.getAndIncrement();
         this.ruleid = ruleid;
         this.created_by_user = created_by_user;
@@ -79,43 +81,4 @@ public class Rule {
         this.ruleid = ruleid;
     }
 
-    private HashMap<String,String> rule_filters;
-
-    @Override
-    public String toString() {
-        return "Rule{" +
-                "ruleid='" + ruleid + '\'' +
-                ", tool='" + tool + '\'' +
-                ", id=" + id +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Rule rule = (Rule) o;
-
-        if (id != rule.id) return false;
-        if (!ruleid.equals(rule.ruleid)) return false;
-        if (created_by_user != null ? !created_by_user.equals(rule.created_by_user) : rule.created_by_user != null)
-            return false;
-        if (edited_by_user != null ? !edited_by_user.equals(rule.edited_by_user) : rule.edited_by_user != null)
-            return false;
-        if (tool != null ? !tool.equals(rule.tool) : rule.tool != null) return false;
-        return rule_filters != null ? rule_filters.equals(rule.rule_filters) : rule.rule_filters == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + ruleid.hashCode();
-        result = 31 * result + (created_by_user != null ? created_by_user.hashCode() : 0);
-        result = 31 * result + (edited_by_user != null ? edited_by_user.hashCode() : 0);
-        result = 31 * result + (tool != null ? tool.hashCode() : 0);
-        result = 31 * result + (rule_filters != null ? rule_filters.hashCode() : 0);
-        return result;
-    }
 }
