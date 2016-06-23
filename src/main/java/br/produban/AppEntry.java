e6a03e3ccbdf6101ac3c295d195dce3694dd62a5
Bera <brunormoura@gmail.com>
2016-06-23 08:17:16 -0300

    App skeleton done

---
+++ src/main/java/br/produban/AppEntry.java
@@ -0,0 +1,42 @@
+package br.produban;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
+
+/**
+ * Hello world!
+ */
+public class AppEntry extends AbstractVerticle {
+    @Override
+    public void start(Future<Void> fut) {
+
+        // Create a router object.
+        Router router = Router.router(vertx);
+
+        // Bind "/" to our hello message - so we are still compatible.
+        router.route("/").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response
+                    .putHeader("content-type", "text/html")
+                    .end("<h1>Working</h1>");
+        });
+
+        vertx
+                .createHttpServer()
+                .requestHandler(router::accept)
+                .listen(
+                        // Retrieve the port from the configuration,
+                        // default to 8080.
+                        config().getInteger("http.port", 8080),
+                        result -> {
+                            if (result.succeeded()) {
+                                fut.complete();
+                            } else {
+                                fut.fail(result.cause());
+                            }
+                        }
+                );
+    }
+}
