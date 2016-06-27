825239f649d3c6e282c23a214d2095baa7deb33a
Bera <brunormoura@gmail.com>
2016-06-27 16:49:59 -0300

    test refactoring rand port and added test to verify if index.html is served

--- src/test/java/br/produban/AppTest.java
+++ src/test/java/br/produban/AppTest.java
@@ -1,6 +1,8 @@
 package br.produban;
 
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
@@ -8,6 +10,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.net.ServerSocket;
 
 
 /**
@@ -18,29 +23,54 @@ public class AppTest
 
 {
     private Vertx vertx;
+    private int port = 0;
 
     @Before
     public void setUp(TestContext context) {
-        vertx = Vertx.vertx();
-        vertx.deployVerticle(AppEntry.class.getName(),
-                context.asyncAssertSuccess());
+
+
+        try {
+            ServerSocket socket = new ServerSocket(0);
+            port = socket.getLocalPort();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        DeploymentOptions options = new DeploymentOptions()
+                .setConfig(new JsonObject().put("http.port", port)
+                );
+        vertx.deployVerticle(AppEntry.class.getName(), options, context.asyncAssertSuccess());
     }
 
     @After
     public void tearDown(TestContext context) {
+
         vertx.close(context.asyncAssertSuccess());
     }
 
     @Test
-    public void testMyApplication(TestContext context) {
-        final Async async = context.async();
+    public void testDummyAppVertXIsWorking(TestContext context) {
 
-        vertx.createHttpClient().getNow(8080, "localhost", "/",
+        final Async async = context.async();
+        vertx.createHttpClient().getNow(port, "localhost", "/",
                 response -> {
                     response.handler(body -> {
                         context.assertTrue(body.toString().contains("Working"));
                         async.complete();
                     });
                 });
+    }
+
+    @Test
+    public void checkThatIndexPageIsServed(TestContext context) {
+        Async async = context.async();
+        vertx.createHttpClient().getNow(port, "localhost", "/assets/index.html", response -> {
+            context.assertEquals(response.statusCode(),200);
+            context.assertEquals(response.headers().get("content-type"), "text/html");
+            response.bodyHandler(body -> {
+                context.assertTrue(body.toString().contains("<title>Queries Available</title>"));
+                async.complete();
+            });
+        });
     }
 }
