e2b300cc6236ee953b3000a6031bb94d2e9f72e1
Pedro Zatta <pedrozatta@gmail.com>
2016-07-20 14:46:48 -0300

    username

--- src/main/java/br/produban/services/UserService.java
+++ src/main/java/br/produban/services/UserService.java
@@ -1,9 +1,12 @@
 package br.produban.services;
 
+import java.util.HashMap;
+
 import org.apache.log4j.Logger;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Service;
 
 /**
@@ -24,9 +27,15 @@ public class UserService {
 		return auth;
 	}
 
+	@SuppressWarnings("unchecked")
 	public String getAuthenticatedUserName() {
-		Authentication auth = getContext().getAuthentication();
-		return auth.getName();
+		OAuth2Authentication auth = (OAuth2Authentication) getContext().getAuthentication();
+		HashMap<String, String> details = (HashMap<String, String>) auth.getUserAuthentication().getDetails();
+		String userName = details.get("sub");
+		if (userName.contains("@")) {
+			userName = userName.split("@")[0];
+		}
+		return userName;
 	}
 
 }
