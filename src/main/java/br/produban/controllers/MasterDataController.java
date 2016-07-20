9a9e04f20c02247b5ca9a14cdb5edbf5fd63410d
Pedro Zatta <pedrozatta@gmail.com>
2016-07-20 15:59:08 -0300

    JIRA CEPW-26

---
+++ src/main/java/br/produban/controllers/MasterDataController.java
@@ -0,0 +1,41 @@
+package br.produban.controllers;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.produban.services.MasterDataService;
+
+/**
+ * Created by pedrozatta
+ */
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/masterdata")
+public class MasterDataController {
+
+	final static Logger logger = Logger.getLogger(MasterDataController.class);
+
+	@Autowired
+	protected MasterDataService masterDataService;
+
+	@ModelAttribute
+	public void setVaryResponseHeader(HttpServletResponse response) {
+		response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+		response.setHeader("Access-Control-Allow-Origin", "*");
+	}
+
+	@RequestMapping(value = "/{tool}/metrics", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public String getCepRule(@PathVariable("tool") String tool) {
+		return masterDataService.findMetricsByTool(tool);
+	}
+
+}
---
+++ src/main/java/br/produban/services/MasterDataService.java
@@ -0,0 +1,31 @@
+package br.produban.services;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by pedrozatta
+ */
+
+@Service
+public class MasterDataService {
+
+	final static Logger logger = Logger.getLogger(MasterDataService.class);
+
+	public String findMetricsByTool(final String tool) {
+
+		InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("tools/" + tool + ".json");
+
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(inputStream).useDelimiter("\\A");
+
+		String json = scanner.hasNext() ? scanner.next() : "";
+
+		return json;
+
+	}
+
+}
