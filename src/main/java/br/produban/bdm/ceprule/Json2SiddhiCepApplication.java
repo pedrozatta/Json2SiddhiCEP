98f52a63f276743a14a8a633bd6985031684dd8d
xb185475 <p000pedro.zatta@prservicos.com.br>
2016-08-09 18:36:06 -0300

    removido ehcache e gemfire cache

--- src/main/java/br/produban/bdm/ceprule/Json2SiddhiCepApplication.java
+++ src/main/java/br/produban/bdm/ceprule/Json2SiddhiCepApplication.java
@@ -2,23 +2,12 @@ package br.produban.bdm.ceprule;
 
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
-import java.util.Properties;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.gemfire.CacheFactoryBean;
-import org.springframework.data.gemfire.LocalRegionFactoryBean;
-import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-
-import com.gemstone.gemfire.cache.GemFireCache;
-
-import br.produban.bdm.ceprule.model.CepRule;
 
 /**
  * Created by pedrozatta
@@ -26,8 +15,6 @@ import br.produban.bdm.ceprule.model.CepRule;
 
 @SpringBootApplication
 @EnableResourceServer
-@EnableCaching
-@EnableGemfireRepositories
 public class Json2SiddhiCepApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) throws KeyManagementException, NoSuchAlgorithmException {
@@ -38,35 +25,6 @@ public class Json2SiddhiCepApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Json2SiddhiCepApplication.class);
-	}
-
-	@Bean
-	Properties gemfireProperties() {
-		Properties gemfireProperties = new Properties();
-		gemfireProperties.setProperty("name", "DataGemFireApplication");
-		gemfireProperties.setProperty("mcast-port", "0");
-		gemfireProperties.setProperty("log-level", "config");
-		return gemfireProperties;
-	}
-
-	@Bean
-	CacheFactoryBean gemfireCache() {
-		CacheFactoryBean gemfireCache = new CacheFactoryBean();
-		gemfireCache.setClose(true);
-		gemfireCache.setProperties(gemfireProperties());
-		
-		return gemfireCache;
-	}
-
-	@Bean
-	LocalRegionFactoryBean<String, CepRule> helloRegion(@Value("${br.produban.gemfire.persistent}") Boolean persistent,
-			final GemFireCache cache) {
-		LocalRegionFactoryBean<String, CepRule> region = new LocalRegionFactoryBean<>();
-		region.setCache(cache);
-		region.setClose(false);
-		region.setName("CepRule");
-		region.setPersistent(persistent);
-		return region;
 	}
 
 }
